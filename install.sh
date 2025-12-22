99eda0af7de33028d3820a6ad2824e9d5bf94ea3
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-12-22 10:32:41 +0100

    waybar style config based on catpuccin

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(stow kitty wofi hyprland hypridle hyprlock hyprpaper waybar)
+PACKAGES=(stow kitty wofi hyprland hypridle hyprlock hyprpaper waybar otf-font-awesome)
 pacman -Syu "${PACKAGES[@]}"
 
 STOWABLES=(kitty hyprland hyprlock waybar hyprpaper wallpapers)
