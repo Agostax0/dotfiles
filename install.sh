76028658836cb3168e963cf884fedc358645ea76
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-12-25 21:14:43 +0100

    hyprlock config and fixes to install

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 PACKAGES=(stow kitty rofi hyprland hypridle hyprlock hyprpaper waybar otf-font-awesome brave-browser wl-clipboard)
 pacman -Syu "${PACKAGES[@]}"
 
-STOWABLES=(kitty hyprland hyprlock waybar hyprpaper wallpapers)
+STOWABLES=(kitty rofi hyprland hypridle hyprlock hyprpaper waybar)
+stow -t ~/ "${STOWABLES[@]}"
 
-stow "${STOWABLES[@]}"
+BASE_FONT=monospace
+sh -c "cd fonts/ && stow -t ~/ $BASE_FONT"
+
+BASE_WALLPAPER=dragon-of-dojima-dark
+sh -c "cd wallpapers/ && stow -t ~/ $BASE_WALLPAPER"
+
+BASE_THEME=mocha
+sh -c "cd themes/ && stow -t ~/ $BASE_THEME"
