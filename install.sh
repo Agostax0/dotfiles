a4cac282c85ef01c7ce056a0aef9daba5c5caa7a
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-12-25 11:07:51 +0100

    using brave browser

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(stow kitty wofi hyprland hypridle hyprlock hyprpaper waybar otf-font-awesome)
+PACKAGES=(stow kitty wofi hyprland hypridle hyprlock hyprpaper waybar otf-font-awesome brave-browser)
 pacman -Syu "${PACKAGES[@]}"
 
 STOWABLES=(kitty hyprland hyprlock waybar hyprpaper wallpapers)
