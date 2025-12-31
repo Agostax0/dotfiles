7f7bba573ce1695f1d09c8c89348455fc0de494c
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-12-31 08:58:02 +0100

    added dir creation to configs

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 PACKAGES=(stow kitty hyprland hypridle hyprlock hyprpaper hyprsunset hyprpicker rofi waybar dunst brightnessctl otf-font-awesome brave-browser wl-clipboard hyprshot)
+NEEDED_DIRS=(kitty rofi hypr waybar dunst)
+STOWABLES=(kitty rofi hyprland hypridle hyprlock hyprpaper waybar hyprshot dunst)
+
+echo "Installing packages"
 pacman -Sy "${PACKAGES[@]}" --needed --noconfirm
 
-STOWABLES=(kitty rofi hyprland hypridle hyprlock hyprpaper waybar hyprshot dunst)
+CONFIG_DIR="$HOME/.config/"
+
+echo "Creating folder in .config"
+for FOLDER in "${NEEDED_DIRS[@]}"; do
+  mkdir -p "$CONFIG_DIR/$FOLDER"
+done
+
+echo "Stowing configs"
 stow -t ~/ "${STOWABLES[@]}"
 
+echo "Installing mocha theme"
 BASE_FONT=monospace
 sh -c "cd fonts/ && stow -t ~/ $BASE_FONT"
 
