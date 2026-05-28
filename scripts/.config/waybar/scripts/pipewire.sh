f97871d36c8961146253ebe6bf5994bf370b922e
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2026-05-28 11:55:22 +0200

    chore: fix conflicts

--- scripts/.config/waybar/scripts/pipewire.sh
+++ scripts/.config/waybar/scripts/pipewire.sh
@@ -11,7 +11,6 @@ snore() {
 
 DELAY=0.2
 
-<<<<<<< HEAD
 INPUT=
 case "$1" in
 "output")
@@ -54,31 +53,6 @@ while snore $DELAY; do
       printf '{"text":"%s", "alt":"%s"}\n' "$VOLUME%" "$ICON"
     else
       printf '{"text":"Error","alt":"invalid volume"}\n'
-=======
-while snore $DELAY; do
-  WP_OUTPUT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-
-  if [[ $WP_OUTPUT =~ ^Volume:[[:blank:]]([0-9]+)\.([0-9]{2})([[:blank:]].MUTED.)?$ ]]; then
-    if [[ -n ${BASH_REMATCH[3]} ]]; then
-      printf "MUTE\n"
-    else
-      VOLUME=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
-      ICON=(
-        ""
-        ""
-        ""
-      )
-
-      if [[ $VOLUME -gt 50 ]]; then
-        printf "%s" "${ICON[0]} "
-      elif [[ $VOLUME -gt 25 ]]; then
-        printf "%s" "${ICON[1]} "
-      elif [[ $VOLUME -ge 0 ]]; then
-        printf "%s" "${ICON[2]} "
-      fi
-
-      printf "$VOLUME%%\n"
->>>>>>> cb03f49 (added audio control)
     fi
   fi
 done
